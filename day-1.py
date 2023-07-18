737fc4d7166017e7c5f47ce59a949dd74f3e4af1
Anton Kävenheim <akaevenheim@gmail.com>
2023-07-18 18:13:38 +0200

    Use built-in functions max/sum instead of reduce

--- day-1.py
+++ day-1.py
@@ -1,23 +1,19 @@
-from functools import reduce
-
-
 with open("./day-1-input.txt", 'r') as f:
 	# list = f.readlines()
 	list = [line.strip() for line in f]
-
-	print("--- Part One ---")
 
 	listOfSums = []
 	tempList = []
 	for i in range(len(list)):
 		if list[i] != "":
 			tempList.append(int(list[i]))			
-		if list[i] == "" or i == len(list) - 1:
-			listOfSums.append(reduce(lambda a, b : a + b, tempList, 0))
+		if list[i] == "" or list[i] is None:
+			listOfSums.append(sum(tempList))
 			tempList = []
-	print(reduce(lambda a, b : a if a > b else b, listOfSums))
 
-	print("-- Part Two ---")
-	
+	print("--- Part One ---")
+	print(max(listOfSums))
+
+	print("-- Part Two ---")	
 	listOfSums.sort(reverse = True)
-	print(reduce(lambda a, b : a + b, listOfSums[0:3]))
+	print(sum(listOfSums[0:3]))
